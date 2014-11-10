92369e95bd3a85e68f00f27824dae543bcd02858
Peter Jenkins <peter.jenkins@csc.fi>
2014-11-10 15:18:28 +0200

    tested ipython. Not much better

--- cli.py
+++ cli.py
@@ -1,14 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # bpython
-import bpdb
+#import bpdb
+
+# iPython
+from IPython.core.debugger import Tracer
 
 # OpenStack libraries
 from novaclient.v1_1 import client
-from novaclient import utils
+#from novaclient import utils
+#from neutronclient.neutron import client
 
 # For reading environment varibles
 import os
+import sys
 
 # Consume standard OpenStack environment variables.
 OS_AUTH_URL=os.environ.get('OS_AUTH_URL', 'http://127.0.0.1:35357/v2.0/')
@@ -19,22 +24,26 @@ OS_TENANT_NAME=os.environ.get('OS_TENANT_NAME', OS_USERNAME)
 
 # Try to connect
 try:
-    c = client.Client(OS_USERNAME, OS_PASSWORD, OS_TENANT_NAME, OS_AUTH_URL)
+    nova = client.Client(OS_USERNAME, OS_PASSWORD, OS_TENANT_NAME, OS_AUTH_URL)
 
     # This should fail if we don't have a working connection
-    c.authenticate()
+    nova.authenticate()
 
 except Exception as e:
-    print "ghostnodes: CRITICAL " + str(e)
-    sys.exit(STATE_CRITICAL)
+    print e
+    sys.exit(1)
 
-help_text = 'About to dump you to the debugger. Use the varible \'c\' to access your connection to openstack.\n' + \
+help_text = 'About to dump you to the debugger. Use the varible \'nova\' to access your connection to openstack.\n' + \
 'For example:\n' + \
 '\n' + \
-'print(c.quotas.get(\'csc\'))' + \
+'print(nova.quotas.get(\'csc\'))' + \
 "<QuotaSet cores=256, fixed_ips=-1, floating_ips=51, injected_file_content_bytes=10240, injected_file_path_bytes=255, injected_files=5, instances=501, key_pairs=100, metadata_items=128, ram=1024000, security_group_rules=100, security_groups=50>"
 
 print(help_text)
 
 # Break out to the bpython enterpretor
-bpdb.set_trace()
+#bpdb.set_trace()
+
+# Break out to iPython
+Tracer()()
+
