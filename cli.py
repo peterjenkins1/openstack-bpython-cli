edee29c8acc8873c80ad8edde12c842f0ec604d9
Peter Jenkins <peter.jenkins@csc.fi>
2014-11-10 15:39:08 +0200

    working with keystone and nova

--- cli.py
+++ cli.py
@@ -4,35 +4,59 @@
 import bpdb
 
 # OpenStack libraries
-from novaclient.v1_1 import client
+import keystoneclient.v2_0.client as keystoneclient
+from neutronclient import client as neutronclient
+from novaclient.v1_1 import client as novaclient
 from novaclient import utils
 
 # For reading environment varibles
 import os
 
-# Consume standard OpenStack environment variables.
-OS_AUTH_URL=os.environ.get('OS_AUTH_URL', 'http://127.0.0.1:35357/v2.0/')
-OS_PASSWORD=os.environ.get('OS_PASSWORD', None)
-OS_REGION_NAME=os.environ.get('OS_REGION_NAME', None)
-OS_USERNAME=os.environ.get('OS_USERNAME', 'admin')
-OS_TENANT_NAME=os.environ.get('OS_TENANT_NAME', OS_USERNAME)
+import sys
 
-# Try to connect
+def get_keystone_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['tenant_name'] = os.environ['OS_TENANT_NAME']
+    return d
+
+def get_nova_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
+    return d
+
+# Read OpenStack environment variables
+nova_creds = get_nova_creds()
+keystone_creds = get_keystone_creds()
+
+# Try to connect to all the clients
 try:
-    c = client.Client(OS_USERNAME, OS_PASSWORD, OS_TENANT_NAME, OS_AUTH_URL)
+    nova = novaclient.Client(**nova_creds)
+    keystone = keystoneclient.Client(**keystone_creds)
+#    neutron = neutronclient.Client(**creds)
 
     # This should fail if we don't have a working connection
-    c.authenticate()
+    nova.authenticate()
+    keystone.authenticate()
+#    neutron.authenticate()
 
 except Exception as e:
-    print "ghostnodes: CRITICAL " + str(e)
-    sys.exit(STATE_CRITICAL)
+    print e
+    sys.exit(1)
 
-help_text = 'About to dump you to the debugger. Use the varible \'c\' to access your connection to openstack.\n' + \
-'For example:\n' + \
-'\n' + \
-'print(c.quotas.get(\'csc\'))' + \
-"<QuotaSet cores=256, fixed_ips=-1, floating_ips=51, injected_file_content_bytes=10240, injected_file_path_bytes=255, injected_files=5, instances=501, key_pairs=100, metadata_items=128, ram=1024000, security_group_rules=100, security_groups=50>"
+help_text = '''About to dump you to the debugger.
+
+Use the varibles nova etc to access your connection to openstack.
+For example:
+
+nova.quotas.get('csc')
+<QuotaSet cores=256, fixed_ips=-1, floating_ips=51, injected_file_content_bytes=10240, injected_file_path_bytes=255, injected_files=5, instances=501, key_pairs=100, metadata_items=128, ram=1024000, security_group_rules=100, security_groups=50>
+'''
 
 print(help_text)
 
