f26076fd536072a6575baf5f17d1271d72d9ff25
Peter Jenkins <peter.jenkins@csc.fi>
2015-03-18 12:56:13 +0200

    Added python-cinderclient dependancy + cleanup
    python-cinderclient was required, but missing from requirements.txt.
    Thanks to @tourunen for reporting.
    
    Added help() and debug() methods. Help displays the usage text, but
    inside bpython. debug() enables python logging to the console which can
    be useful for debugging connection issues or understanding what REST
    api the python code is actually using (it would be nice if one never
    need to do this!).
    
    Cleaned up the code a bit.

--- cli.py
+++ cli.py
@@ -14,8 +14,24 @@ from neutronclient.neutron import client as neutronclient
 
 # For reading environment varibles
 import os
-
 import sys
+
+# To see errors from the libraries
+import logging
+
+help_text = '''OpenStack bpython cli
+
+Commands:
+
+help_me()  - see this text again.
+debug() - Enable debug logging
+
+Use the varibles nova, neutron, keystone etc to access your connection to openstack.
+For example:
+
+nova.quotas.get('csc')
+<QuotaSet cores=256, fixed_ips=-1, floating_ips=51, injected_file_content_bytes=10240, injected_file_path_bytes=255, injected_files=5, instances=501, key_pairs=100, metadata_items=128, ram=1024000, security_group_rules=100, security_groups=50>
+'''
 
 def get_keystone_creds():
     d = {}
@@ -31,7 +47,15 @@ def get_nova_creds():
     d['api_key'] = os.environ['OS_PASSWORD']
     d['auth_url'] = os.environ['OS_AUTH_URL']
     d['project_id'] = os.environ['OS_TENANT_NAME']
+    d['service_type'] = 'compute'
     return d
+
+def debug(level=logging.DEBUG):
+  logging.basicConfig(level=level)
+
+def help_me():
+  print(help_text)
+
 
 # Read OpenStack environment variables
 nova_creds = get_nova_creds()
@@ -39,13 +63,13 @@ keystone_creds = get_keystone_creds()
 
 # Try to connect to all the clients
 try:
-    print 'Connecting to nova'
-    nova = novaclient.Client(**nova_creds)
-    nova.authenticate()
-
     print 'Connecting to keystone'
     keystone = keystoneclient.Client(**keystone_creds)
     keystone.authenticate()
+
+    print 'Connecting to nova'
+    nova = novaclient.Client(**nova_creds)
+    nova.authenticate()
 
     print 'Connecting to glance'
     glance_endpoint = keystone.service_catalog.url_for(service_type='image',
@@ -64,16 +88,7 @@ except Exception as e:
     print e
     sys.exit(1)
 
-help_text = '''About to dump you to the debugger.
-
-Use the varibles nova etc to access your connection to openstack.
-For example:
-
-nova.quotas.get('csc')
-<QuotaSet cores=256, fixed_ips=-1, floating_ips=51, injected_file_content_bytes=10240, injected_file_path_bytes=255, injected_files=5, instances=501, key_pairs=100, metadata_items=128, ram=1024000, security_group_rules=100, security_groups=50>
-'''
-
-print(help_text)
+help_me()
 
 # Break out to the bpython enterpretor
 bpdb.set_trace()
